035484cf175d67bcbe9207a5bdc2955416d184f3
Oscar Garcia <oscar615@revature.net>
2022-06-19 16:20:50 -0500

    reorganized classes

--- input.py
+++ input.py
@@ -1,6 +1,102 @@
 import csv
+from datetime import datetime
+from csvWrite import write
+from database import sql
+from misc import phone
+from show import show
 
 #class with functions to create csv files based on the user input
 
 
+class insert(sql):
 
+
+    def insertCustomer(self):
+        header = ['fname', 'lname', 'phone', 'Address']
+
+        fname, lname = input("Enter First and Last Name: ").split()
+        phon = phone().input()
+        Address = input("Enter Address: ")
+
+        data = [fname, lname, phon, Address]
+
+        with open('./csv/customer.csv', 'w', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+
+            # write the header
+            writer.writerow(header)
+
+            # write the data
+            writer.writerow(data)
+
+        write().customer()
+
+
+    def getval(self):
+        while(True):
+            try:
+                print("Vegan-1")
+                print("Non-Vegan")
+                option = int(input("please make a choice >>"))
+            except:
+                print('Wrong input. Please enter a number ...')
+            if option == 1:
+                return 'Veg'
+            elif option == 2:
+                return 'Non-Veg'
+            else:
+                print('Invalid option. Please enter a number between 1 or 2.')
+
+
+    def insertItems(self):
+        header = ['Name','Price','Type','Category']
+        name = input("Food Name: ")
+        Price = round((float(input("Price: "))),2)
+        Type = self.getval()
+        Category = input("Category: ")
+
+        data = [name, Price, Type, Category]
+
+        with open('./csv/items.csv', 'w', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+
+            # write the header
+            writer.writerow(header)
+
+            # write the data
+            writer.writerow(data)
+
+        write().items()
+
+
+
+    def insertOrder(self):
+        header = ['id_customer','id_item','OrderDate']
+        show().showCustomer()
+        numLimit = sql().query("SELECT id_Customer FROM customer ORDER BY id_Customer ASC")
+        idCust=0
+        while not int(idCust) in numLimit:
+            idCust = input("Please enter Customer ID : ")
+        
+        show().showItems()
+        numLimit = sql().query("SELECT id_item FROM items ORDER BY id_item ASC")
+        itemID=0
+        while not int(itemID) in numLimit:
+            itemID = input("Please Item ID  : ")
+        
+        date = datetime.today().strftime('%Y-%m-%d')
+
+
+
+        data = [idCust, itemID, date]
+
+        with open('./csv/orders.csv', 'w', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+
+            # write the header
+            writer.writerow(header)
+
+            # write the data
+            writer.writerow(data)
+
+        write().orders()
--- menu.py
+++ menu.py
@@ -1,23 +1,21 @@
 from rich.console import Console
 from os import system
-from database import query
 from collections import namedtuple
 import re
-import string
-from misc import phone
-
+from delete import delete
+from input import insert
+from show import show
 
 c = Console()
-dp = query()
-#dp.query("SELECT * FROM customer")
 
 def menu():
-    c.print("-------------------",style='light_sea_green')
+    c.print("--------Main Menu-----------",style='light_sea_green')
     c.print("Select an option")
     c.print("Customer Menu 1")
     c.print("Items Menu 2")
-    c.print("Take New Order 3")
-    c.print("EXIT 4")
+    c.print("Orders Menu 3")
+    c.print("Take New Order 4")
+    c.print("EXIT 5")
     c.print("-------------------")
 
 def customer_menu():
@@ -29,7 +27,27 @@ def customer_menu():
     c.print("Modify Customer 4")
     c.print("Back to main menu 5")
     c.print("-------------------")
-    
+
+def items_menu():
+    c.print("-----Items menu--------------",style='light_sea_green')
+    c.print("Select an option")
+    c.print("Show Items 1")
+    c.print("Add Items 2")
+    c.print("Delete Item 3")
+    c.print("Modify Item 4")
+    c.print("Back to main menu 5")
+    c.print("-------------------")
+
+def orders_menu():
+    c.print("-----Orders menu--------------",style='light_sea_green')
+    c.print("Select an option")
+    c.print("Show Orders 1")
+    c.print("Take New Order 2")
+    c.print("Delete Orders 3")
+    c.print("Modify Orders 4")
+    c.print("Back to main menu 5")
+    c.print("-------------------")
+
 
 def cusOption():
     while(True):
@@ -43,47 +61,94 @@ def cusOption():
             #Check what choice was entered and act accordingly
         if option == 1:
             system('cls')
-            dp.showCustomer()
-
+            show().showCustomer()
            
         elif option == 2:
             system('cls')
+            insert().insertCustomer()
+            system('cls')
             
-            fname, lname = input("Enter two values: ").split()
-            phon = phone().input()
-            Address = input("Enter Address")
-            customer= (fname, lname, phon, Address)
-
-            #dp.test("INSERT INTO customer (fname, lname, phone, Address) VALUES "+ c._fields)
-            print("INSERT INTO customer (fname, lname, phone, Address) VALUES "+ str(customer))
-
-
-
+            show().showCustomer()
 
         elif option == 3:
             system('cls')
-            # show cutomers 
-
-
-
+            delete().delCustomer()
 
         elif option == 4:
             system('cls')
 
 
         elif option == 5:
-            print('Thanks message before exiting')
-            exit()
+            system('cls')
+            break
         else:
-            print('Invalid option. Please enter a number between 1 and 4.')    
+            print('Invalid option. Please enter a number between 1 and 5.')    
 
 
-def option2():
-     print('Handle option \'Option 2\'')
+def itemOption():
+    while(True):
+        #print('Handle option \'Option 1\'')
+        items_menu()
+        option = ''
+        try:
+            option = int(input("please make a choice >>"))
+        except:
+            print('Wrong input. Please enter a number ...')
+            #Check what choice was entered and act accordingly
+        if option == 1:
+            system('cls')
+            show().showItems()
+           
+        elif option == 2:
+            system('cls')
+            insert().insertItems()
+            system('cls')
+            show().showItems()
 
-def option3():
-     print('Handle option \'Option 3\'')
+        elif option == 3:
+            system('cls')
+            delete().delItems()
 
+        elif option == 4:
+            system('cls')
+
+        elif option == 5:
+            system('cls')
+            break
+        else:
+            print('Invalid option. Please enter a number between 1 and 5.')    
+
+def orderOption():
+    while(True):
+        #print('Handle option \'Option 1\'')
+        orders_menu()
+        option = ''
+        try:
+            option = int(input("please make a choice >>"))
+        except:
+            print('Wrong input. Please enter a number ...')
+            #Check what choice was entered and act accordingly
+        if option == 1:
+            system('cls')
+            show().showOrders()
+           
+        elif option == 2:
+            system('cls')
+            insert().insertOrder()
+            print("Order Added")
+
+        elif option == 3:
+            system('cls')
+            delete().delOrders()
+
+        elif option == 4:
+            system('cls')
+
+        elif option == 5:
+            system('cls')
+            break
+        else:
+            print('Invalid option. Please enter a number between 1 and 5.')   
 
 
 system('cls')
@@ -103,13 +168,18 @@ if __name__=='__main__':
            
         elif option == 2:
             system('cls')
-            option2()
+            itemOption()
 
         elif option == 3:
             system('cls')
-            option3()
+            orderOption()
+
         elif option == 4:
-            print('Thanks message before exiting')
+            system('cls')
+            insert().insertItems()
+
+        elif option == 5:
+            print('Now exiting')
             exit()
         else:
-            print('Invalid option. Please enter a number between 1 and 4.')
+            print('Invalid option. Please enter a number between 1 and 5.')
